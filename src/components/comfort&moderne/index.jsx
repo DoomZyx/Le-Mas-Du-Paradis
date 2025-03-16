3455cf8493ff373e49e24da755ae4f63d5723a93
DoomZyx <axelcella.ac@gmail.com>
2025-03-16 16:10:23 +0100

    section comfort ajouté

---
+++ src/components/comfort&moderne/index.jsx
@@ -0,0 +1,20 @@
+import "./_index.scss";
+
+function Comfort() {
+  return (
+    <>
+      <div className="comf-layout">
+        <h2>Confort et modernité 🛋️</h2>
+        <p>
+          Nous avons séléctionné chaque meuble et chaque élément de décoration
+          avec soin 🪑
+          <br /> <br />
+          Tout a été acheté neuf, pour offrir un cadre chaleureux et morderne,
+          tout en respectant l’ésthétique authentique de la pierre
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Comfort;
--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -3,6 +3,7 @@ import Header from "../../components/Header-A/header";
 import Story from "../../components/Story/story";
 import Serenity from "../../components/Serenity/serinity";
 import Renovation from "../../components/Renovation /renovation";
+import Comfort from "../../components/comfort&moderne";
 import "./_about.scss";
 
 function About() {
@@ -14,6 +15,7 @@ function About() {
         <Story />
         <Serenity />
         <Renovation />
+        <Comfort />
       </main>
     </>
   );
