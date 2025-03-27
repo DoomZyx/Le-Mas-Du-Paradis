0a154a4b6b077c19f23e35ba737d85016ac044bf
DoomZyx <axelcella.ac@gmail.com>
2025-03-27 19:41:10 +0100

    Ajout de formSubmit

--- src/components/Contact/Form/form.jsx
+++ src/components/Contact/Form/form.jsx
@@ -1,11 +1,37 @@
 import "./_form.scss";
+import { useState } from "react";
 
 function Form() {
+  const [messageSent, setMessageSent] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+       // Vérifie le champ honeypot (doit rester vide) (Evite les spams des bots)
+       if (formData.get("honeypot") !== "") {
+        return; // bot détecté, on annule
+      }
+
+    fetch(`https://formsubmit.co/${import.meta.env.VITE_FORMSUBMIT_EMAIL}`, {
+      method: "POST",
+      body: formData,
+    }).then((response) => {
+      if (response.ok) {
+        setMessageSent(true);
+        e.target.reset(); 
+      }
+    });
+  };
   return (
     <>
       <h2 className="form-title">Contactez-nous 📩</h2>
-      <p className="form-desc">N'hésitez pas à nous contacter par mail our par téléphone pour tout renseignement ou pour réserver </p>
-      <form>
+      <p className="form-desc">
+        N'hésitez pas à nous contacter par mail our par téléphone pour tout
+        renseignement ou pour réserver{" "}
+      </p>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="name">Nom</label>
           <input type="text" id="name" placeholder="Nom" required />
@@ -24,10 +50,20 @@ function Form() {
         </div>
         <div className="form-group">
           <label htmlFor="message">Votre Message</label>
-          <textarea id="message" rows="4" placeholder="Ecrivez votre demande" required></textarea>
+          <textarea
+            id="message"
+            rows="4"
+            placeholder="Ecrivez votre demande"
+            required
+          ></textarea>
         </div>
         <button type="submit">Envoyer</button>
       </form>
+      {messageSent && (
+        <p className="confirmation-message">
+          Votre message a bien été envoyé à l'hôte !
+        </p>
+      )}
     </>
   );
 }
